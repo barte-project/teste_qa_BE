57d669b19e19af394dfc9ea8e97890ea6097eaad
rafael suzano <rafael.suzano@barte.com>
2023-11-09 15:37:28 -0300

    add  report html
    add  report html

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,23 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
  
-  baseurl:'https://hml-bff.barte.com/',
+
+    baseurl:'https://hml-bff.barte.com/',
+
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: 'BARTE',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
+
+  },
+
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
